07d5c4f314d78362266af499ee41d9e92138a960
Vincent Untz <vuntz@gnome.org>
2011-01-31 17:30:46 +0100

    na: Move creating of GtkBox for NaTray
    Do it in init() instead of constructor(), for consistency. It shouldn't
    matter much, really.
    
    Conflicts:
    	applets/notification_area/na-tray.c

--- applets/notification_area/na-tray.c
+++ applets/notification_area/na-tray.c
@@ -84,27 +84,6 @@ static TraysScreen *trays_screens = NULL;
 
 static void icon_tip_show_next (IconTip *icontip);
 
-#if !GTK_CHECK_VERSION (3, 0, 0)
-/* NaBox, an instantiable GtkBox */
-
-typedef GtkBox      NaBox;
-typedef GtkBoxClass NaBoxClass;
-
-static GType na_box_get_type (void);
-
-G_DEFINE_TYPE (NaBox, na_box, GTK_TYPE_BOX)
-
-static void
-na_box_init (NaBox *box)
-{
-}
-
-static void
-na_box_class_init (NaBoxClass *klass)
-{
-}
-#endif
-
 /* NaTray */
 
 G_DEFINE_TYPE (NaTray, na_tray, GTK_TYPE_BIN)
@@ -540,7 +519,7 @@ na_tray_expose_icon (GtkWidget *widget,
 #endif
 		     gpointer   data)
 {
-  cairo_t *cr = data;
+  cairo_t *cr = (cairo_t *) data;
 
   if (na_tray_child_has_alpha (NA_TRAY_CHILD (widget)))
     {
